0639faf481d2d96a3c81c920f3875b8eabf4fab5
Santiago <santiago.gela@gmail.com>
2019-03-12 14:46:28 -0300

    chromium browser

--- tests/demo.js
+++ tests/demo.js
@@ -15,7 +15,7 @@ after(browser){
 		browser
 		.assert.title("Google")
 		.waitForElementVisible('body', 25000)
-		.setValue('input[title=Buscar]', 'Hello')
+		.setValue('input.gLFyf.gsfi', 'Hello')
 		.assert.containsText('.erkvQe:first-child', 'hello')
 	},
 
